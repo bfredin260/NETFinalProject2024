86820f0949b85dd9eb96468160b6237e50a44cbd
Braedon Fredin <b260fredin@gmail.com>
2024-05-11 22:42:54 -0500

    [1.18] Added ability to add new Product.

--- Model/Product.cs
+++ Model/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NWConsole.Model
 {
@@ -11,16 +12,22 @@ namespace NWConsole.Model
         }
 
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Needs a name")]
         public string ProductName { get; set; }
         public int? SupplierId { get; set; }
         public int? CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Enter quantity per unit")]
         public string QuantityPerUnit { get; set; }
+
+        [Required(ErrorMessage = "Enter unit price")]
         public decimal? UnitPrice { get; set; }
         public short? UnitsInStock { get; set; }
         public short? UnitsOnOrder { get; set; }
         public short? ReorderLevel { get; set; }
         public bool Discontinued { get; set; }
 
+        [Required(ErrorMessage = "Product needs Category")]
         public virtual Category Category { get; set; }
         public virtual Supplier Supplier { get; set; }
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using NWConsole.Model;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 // See https://aka.ms/new-console-template for more information
 string path = Directory.GetCurrentDirectory() + "/nlog.config";
@@ -22,6 +23,7 @@ try
         Console.WriteLine("2) Add Category");
         Console.WriteLine("3) Display Category and related products");
         Console.WriteLine("4) Display all Categories and their related products");
+        Console.WriteLine("5) Add new Product");
         Console.WriteLine("\"q\" to quit");
         choice = Console.ReadLine();
         Console.Clear();
@@ -62,7 +64,8 @@ try
                 else
                 {
                     logger.Info("Validation passed");
-                    // TODO: save category to db
+                    db.Categories.Add(category);
+                    db.SaveChanges();
                 }
             }
             if (!isValid)
@@ -103,6 +106,56 @@ try
                 foreach (Product p in item.Products)
                 {
                     Console.WriteLine($"\t{p.ProductName}");
+                }
+            }
+        }
+        else if (choice == "5")
+        {
+                Product product = new Product();
+                Console.WriteLine("Enter Product Name:");
+                product.ProductName = Console.ReadLine();
+                Console.WriteLine("What category is this product in?");
+                foreach(Category c in db.Categories) {
+                    Console.WriteLine($"{c.CategoryId}) {c.CategoryName}");
+                }
+                try{
+                    product.Category = db.Categories.FirstOrDefault(c => c.CategoryId == int.Parse(Console.ReadLine()));
+                } catch(Exception e) {
+                    Console.WriteLine(e.Message);
+                }
+                Console.WriteLine("Enter Quantity Per Unit:");
+                product.QuantityPerUnit = Console.ReadLine();
+                Console.WriteLine("Enter Price Per Unit:");
+            try {
+                product.UnitPrice = decimal.Parse(Console.ReadLine());
+            } catch(Exception e) {
+                logger.Error(e.Message);
+            }
+            ValidationContext context = new ValidationContext(product, null, null);
+            List<ValidationResult> results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(product, context, results, true);
+            if (isValid)
+            {
+                // check for unique name
+                if (db.Products.Any(p => p.ProductName == product.ProductName))
+                {
+                    // generate validation error
+                    isValid = false;
+                    results.Add(new ValidationResult("Name exists", new string[] { "ProductName" }));
+                }
+                else
+                {
+                    logger.Info("Validation passed");
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                }
+            }
+            if (!isValid)
+            {
+                foreach (var result in results)
+                {
+                    logger.Error($"{result.MemberNames.First()} : {result.ErrorMessage}");
                 }
             }
         }
